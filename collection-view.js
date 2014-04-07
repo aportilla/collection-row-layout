6cff9288c6e3fe19e8ee94414d93131dfb578c13
Adam Portilla <adam.portilla@gmail.com>
2014-04-06 20:23:09 -0700

    Fine tune layout strategy

--- collection-view.js
+++ collection-view.js
@@ -6,39 +6,35 @@ var COLLECTION_VIEW = (function(){
 
     currentItems = [],
 
-    imgMargin = 10,
-
     handleResize = _.throttle(function(){
 
       var currentPageWidth = $window.width();
       var targetRowAspect = 
-        currentPageWidth < 400 ? 1 : 
-        currentPageWidth < 700 ? 2 : 
-        currentPageWidth < 1000 ? 3 : 
-        currentPageWidth < 1300 ? 4 : 5;
+        currentPageWidth < 300 ? 1 : 
+        currentPageWidth < 800 ? 2 : 
+        currentPageWidth < 1200 ? 3 : 
+        currentPageWidth < 2000 ? 4 : 5;
+      var imgMargin = 12;
       var itemCount = currentItems.length;
       var item;
-      var currentTop = 0;
+      var currentTop = imgMargin;
       var currentRowItems = [];
       var rowSumAspect = 0;
-      var halfMargin = imgMargin / 2;
       var rowHeight = 200;
 
       var injectRow = function(isDynamicRowHeight){
-
         var frag = document.createDocumentFragment();
         var rowItemCount = currentRowItems.length;
         var adjustedRowWidth = currentPageWidth - (rowItemCount * imgMargin) - imgMargin;
         var leftPos = imgMargin;
-
         if (isDynamicRowHeight){
           rowHeight = adjustedRowWidth / rowSumAspect; 
         }
-
         for (var i = 0; i < rowItemCount; i++) {
           var rowItem = currentRowItems[i];
-          var itemAspect = (rowItem.width + imgMargin) / (rowItem.height + imgMargin);
+          var itemAspect = rowItem.width / rowItem.height;
           var renderWidth = Math.floor(itemAspect * rowHeight);
+          frag.appendChild(rowItem.el);
           rowItem.$el.css({
             height : Math.floor(rowHeight),
             width : renderWidth,
@@ -46,30 +42,22 @@ var COLLECTION_VIEW = (function(){
             left : leftPos
           });
           leftPos += renderWidth + imgMargin;
-          frag.appendChild(rowItem.el);
         }
-
         currentTop += Math.floor(rowHeight + imgMargin);
         $el.append(frag);
         currentRowItems = [];
         rowSumAspect = 0;
-
       };
 
       for (var i = 0; i < itemCount; i++) {
-
         item = currentItems[i];
-
         if (!item.$el){
-          item.$el = $('<div class="item"><div class="frame"><img src="img/' + item.filename + '"/></div></div>');
+          item.$el = $('<div class="item"><div class="frame"><img src="img/' + item.filename + '"/></div><div class="meta"><p>' + item.filename + '</p><p class="sub">' + item.width + ' x ' + item.height + '</p></div></div>');
           item.el = item.$el[0];
-          // item.$frame = item.$el.find('.frame');
         }
-
         var itemAspect = item.width / item.height;
         rowSumAspect += itemAspect;
         currentRowItems.push(item);
-
         // should we inject a row?
         if (rowSumAspect >= targetRowAspect){
           injectRow(true);
